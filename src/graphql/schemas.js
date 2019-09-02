cb343a2c7d936c3bef3d74d748ae3fb4bf3fbf13
angel <angel.elizalde@stanzaresidencial.com>
2019-09-02 11:44:34 -0600

    Se quita autorization de doLogin

--- src/graphql/schemas.js
+++ src/graphql/schemas.js
@@ -172,8 +172,8 @@ const typeDefs = gql`
 
 
   type Mutation {
-    doLogin(email: String, password: String) : Token @AuthDirective
-    addUsuario(data: UsuarioInput) : Token
+    doLogin(email: String, password: String) : Token
+    addUsuario(data: UsuarioInput) : Token @AuthDirective
     updateUsuario(data: UsuarioInput, usuarioID: ID) : Usuario @AuthDirective
     deleteUsuario(usuarioID: ID) : Usuario @AuthDirective
     addRepartidor(data: RepartidorInput) : Repartidor @AuthDirective
